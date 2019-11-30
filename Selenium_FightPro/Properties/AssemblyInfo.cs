ef8bb018470394c83826596fb88349850118518b
Anup <Anup.Damodaran@team.telstra.com>
2019-11-30 14:46:51 +1100

    Add project files.

---
+++ Selenium_FightPro/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Selenium_FightPro")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Telstra Corporation")]
+[assembly: AssemblyProduct("Selenium_FightPro")]
+[assembly: AssemblyCopyright("Copyright © Telstra Corporation 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac5e32c6-e93e-4a97-a96c-b24c0c5acd9b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
