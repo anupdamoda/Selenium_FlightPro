14118d3abfbfa0f03d6f6abe4eacd0e01b54daaa
Anup <Anup.Damodaran@team.telstra.com>
2019-11-30 15:25:04 +1100

    Created 5 Testclasses related to SmokeTests

---
+++ Selenium_FightPro/Testcases/SmokeTests/TS05_PlanningBoard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_FightPro.Testcases.SmokeTests
+{
+    class TS05_PlanningBoard
+    {
+    }
+}
